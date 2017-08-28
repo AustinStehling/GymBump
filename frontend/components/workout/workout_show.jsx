34323f9d216d4f73328887c991f69d3b38e895b1
Austin Stehling <austin.stehling@gmail.com>
2017-08-28 17:11:01 -0400

    Added styling to workout show LIs

--- frontend/components/workout/workout_show.jsx
+++ frontend/components/workout/workout_show.jsx
@@ -8,22 +8,22 @@ class WorkoutShow extends React.Component {
   render () {
     const setArray = values(this.props.selectedWorkout.setresults)
     const exercises = this.props.exercises
-    const results = setArray.map(result => {
+    const results = setArray.map((result, idx) => {
       if (result.workout_id === this.props.selectedWorkout.id) {
         return <li key={result.id} className='workout-show-li'>
-                   <p className='workout-title'>{exercises[result.exercise_id].exercise_name}</p>
+                   <p className='workout-title'><p>Set {idx + 1}: </p><p>{exercises[result.exercise_id].exercise_name}</p></p>
                    <ul>
                      {result.weight_lifted ? (
-                       <li className='workout-result-li'><p>Weight:</p>{result.weight_lifted}{result.weight_unit}</li>
+                       <li className='workout-result-li'><p className='workout-result-li'>Weight:</p>{result.weight_lifted}{result.weight_unit}</li>
                      ) : null}
                      {result.reps ? (
-                       <li className='workout-result-li'><p>Reps:</p>{result.reps}</li>
+                       <li className='workout-result-li'><p className='workout-result-li'>Reps:</p>{result.reps}</li>
                      ) : null}
                      {result.distance ? (
-                       <li className='workout-result-li'><p>Distance:</p>{result.distance}{result.distance_unit}</li>
+                       <li className='workout-result-li'><p className='workout-result-li'>Distance:</p>{result.distance}{result.distance_unit}</li>
                      ) : null}
                      {result.hour || result.min || result.sec ? (
-                       <li className='workout-result-li'><p>Duration:</p>
+                       <li className='workout-result-li'><p className='workout-result-li'>Duration:</p>
                          <div className='dur-format'>
                            {result.hour ? (
                              <p className='dur-result-hour'>{result.hour}:</p>
