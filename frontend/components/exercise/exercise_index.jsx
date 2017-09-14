078f88c5ef1fd8ce0744d43d410b23c37adcd567
Austin Stehling <austin.stehling@gmail.com>
2017-09-14 01:10:21 -0400

    Test for heroku

--- frontend/components/exercise/exercise_index.jsx
+++ frontend/components/exercise/exercise_index.jsx
@@ -34,13 +34,15 @@ class ExerciseIndex extends React.Component {
     let allExercises = this.props.allExercises;
     let selected;
     let name;
-    allExercises.forEach(exercise => {
-      if (exercise.exercise_name === this.state.inputVal) {
-        selected = exercise,
-        name = exercise.exercise_name
-      }
-    })
-    e.preventDefault();
+    if (allExercises) {
+      allExercises.forEach(exercise => {
+        if (exercise.exercise_name === this.state.inputVal) {
+          selected = exercise,
+          name = exercise.exercise_name
+        }
+      })
+      e.preventDefault();
+    }
     if (!name) {
       this.setState({inputVal: 'Invalid Input, Please try Again'})
       return 'Invalid Input'
@@ -50,63 +52,63 @@ class ExerciseIndex extends React.Component {
     this.props.requestAllExercises();
   }
 
-  // getInitialState: function(){
-  //   return {
-  //     allExercises: []
-  //   }
-  // },
-
 
   render() {
 
 
-    let allExercises = this.props.allExercises;
+    let allExercises = this.props.allExercises || [{ exercise_name: '' }]
 
+      let match = allExercises.map((exercise) => {
+        if (this.state.inputVal === '') return [];
+        let matched = [];
+        if (this.state.inputVal.length > 0) {
+          for (var j = 0; j < this.state.inputVal.length; j++) {
+            matched = [];
+            if (exercise.exercise_name.slice(0, j + 1).toUpperCase() === this.state.inputVal.slice(0, j + 1).toUpperCase()) {
+              matched.push(<li onClick={this.handleClick}
+                               value={exercise.exercise_name}
+                               className="workout-auto-li"
+                               key={exercise.id}>{exercise.exercise_name}</li>);
+            }
+          }
+        } else {
+            matched.push(<li onClick={this.handleClick}
+                             value={exercise.exercise_name}
+                             className="workout-auto-li"
+                             key={exercise.id}>{exercise.exercise_name}</li>)
+        }
+        return matched;
+      });
 
-    // const match = allExercises.map((exercise) => {
-    //   if (this.state.inputVal === '') return [];
-    //   let matched = [];
-    //   if (this.state.inputVal.length > 0) {
-    //     for (var j = 0; j < this.state.inputVal.length; j++) {
-    //       matched = [];
-    //       if (exercise.exercise_name.slice(0, j + 1).toUpperCase() === this.state.inputVal.slice(0, j + 1).toUpperCase()) {
-    //         matched.push(<li onClick={this.handleClick}
-    //                          value={exercise.exercise_name}
-    //                          className="workout-auto-li"
-    //                          key={exercise.id}>{exercise.exercise_name}</li>);
-    //       }
-    //     }
-    //   } else {
-    //       matched.push(<li onClick={this.handleClick}
-    //                        value={exercise.exercise_name}
-    //                        className="workout-auto-li"
-    //                        key={exercise.id}>{exercise.exercise_name}</li>)
-    //   }
-    //   return matched;
-    // });
 
     return (
       <div>
-        {this.state.active === 'FIRST' ? (
-          <div className="exercise-main-div">
-            <div className="exercise-second-div">
-              <label className="exercise-label">
-                <h3>Add an Exercise for {this.props.liftname}</h3>
-                <input type="text" value={this.state.inputVal}
-                  onChange={this.handleChange}
-                  className="exercise-input"
-                  />
-              </label>
-            
-              <button className="new-exercise-button"
-                onClick={this.handleSubmit}>Add Exercise</button>
-            </div>
+        {this.props.allExercises ? (
+          <div>
+            {this.state.active === 'FIRST' ? (
+              <div className="exercise-main-div">
+                <div className="exercise-second-div">
+                  <label className="exercise-label">
+                    <h3>Add an Exercise for {this.props.liftname}</h3>
+                    <input type="text" value={this.state.inputVal}
+                      onChange={this.handleChange}
+                      className="exercise-input"
+                      />
+                  </label>
+                  <ul className="exercise-ul">
+                    {match}
+                  </ul>
+                  <button className="new-exercise-button"
+                    onClick={this.handleSubmit}>Add Exercise</button>
+                </div>
+              </div>
+            ) : this.state.active === 'SECOND' ? (
+              <SetResultContainer user={this.props.user}
+                exercises={this.props.exercises}
+                exercise={this.state.name}
+                liftname={this.props.liftname}/>
+            ) : null }
           </div>
-        ) : this.state.active === 'SECOND' ? (
-          <SetResultContainer user={this.props.user}
-            exercises={this.props.exercises}
-            exercise={this.state.name}
-            liftname={this.props.liftname}/>
         ) : null }
       </div>
     );
